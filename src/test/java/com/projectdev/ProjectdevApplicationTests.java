a8d0d54ef33b6fc113bc8380f429ad2c8dbc3c91
karthisivakumar-ux <karthisk12305@gmail.com>
2024-11-22 10:34:52 +0530

    first-commit

---
+++ src/test/java/com/projectdev/ProjectdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
